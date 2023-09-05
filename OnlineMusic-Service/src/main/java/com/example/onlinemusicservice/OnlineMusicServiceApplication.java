30649f49d075d9b0d0ed160dc21c64879b20c980
秋夜泽雨 <2094194909@qq.com>
2023-09-05 18:30:33 +0800

    创建在新音乐项目

---
+++ OnlineMusic-Service/src/main/java/com/example/onlinemusicservice/OnlineMusicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinemusicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMusicServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMusicServiceApplication.class, args);
+    }
+
+}
---
+++ OnlineMusic-Service/src/test/java/com/example/onlinemusicservice/OnlineMusicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinemusicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMusicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
